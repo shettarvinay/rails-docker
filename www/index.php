2ad969fd05ce32f8d35ca0759345b86757f4ceb4
shettarvinay@tcs.com <shettarvinay@tcs.com>
2017-08-14 00:55:41 +0530

    CI/CD achieved

--- www/index.php
+++ www/index.php
@@ -18,6 +18,7 @@
 <body>
 	<img id="logo" src="logo.png" />
 	<h1><?php echo "Hello ".($_ENV["NAME"]?$_ENV["NAME"]:"world")."!"; ?></h1>
+	<h2>CI/CD achieved :) :) </h2>
 	<?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?></h3><?php } ?>
 	<?php
 	$links = [];
